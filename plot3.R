42c54ba46c169b4cdea9a12d7113635448e7d006
Lucia <lucia1119@gmail.com>
2018-02-07 23:03:31 +0800

    make plot3

---
+++ plot3.R
@@ -0,0 +1,42 @@
+# download and unzip the file
+fileUrl="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if (!file.exists("household_power_consumption.txt")) {
+  download.file(fileUrl, destfile="exdata_data_household_power_consumption.zip")
+  file<-unzip(zipfile="exdata_data_household_power_consumption.zip")
+}
+
+
+library(dplyr)
+
+# read txt file as data table
+originalTxt=read.table("household_power_consumption.txt",sep=";",
+                       header = TRUE,stringsAsFactors = FALSE)
+
+
+# using data from the dates 2007-02-01 and 2007-02-02
+date_filter=grep("^(2/2/2007)|^(1/2/2007)",originalTxt$Date)
+graphDf=originalTxt[date_filter,]
+
+
+# combine date and time and transform into date class 
+dateTime=strptime(paste(graphDf$Date,graphDf$Time),format = "%Y-%m-%d %H:%M:%S")
+graphDf$dateTime=dateTime
+
+
+# make the plot
+png('plot3.png',width=480,height=480,units="px",bg = "transparent")
+
+plot(graphDf$dateTime,graphDf$Sub_metering_1,
+     type = "n",
+     bg="transparent",
+     xlab = "",
+     ylab = "Energy sub metering")
+lines(graphDf$dateTime, graphDf$Sub_metering_1, col="black")
+lines(graphDf$dateTime, graphDf$Sub_metering_2, col="red")
+lines(graphDf$dateTime, graphDf$Sub_metering_3, col="blue")
+legend("topright",
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col = c("black","red","blue"),
+       lty =c(1,1,1) )
+dev.off()
